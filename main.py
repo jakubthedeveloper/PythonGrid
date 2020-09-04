b783f54dc62c471bcade62fda0cfb390d10a2b6b
Jakub Krysakowski <jakub.krysakowski@gmail.com>
2020-09-04 08:24:18 +0200

    draw from 2d list

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ screen = pygame.display.set_mode((width, height)) # Making of the screen
 
 gridSize = 10
  
-def grid():
+def drawGrid():
   for y in range(1, gridSize):
     pygame.draw.line(screen, white, (0, (height / gridSize) * y), (width, (height / gridSize) * y), 1)
     
@@ -15,18 +15,33 @@ def grid():
     pygame.draw.line(screen, white, ((width / gridSize) * x, 0), ((width / gridSize) * x, height), 1)
       
 def fillCell(x, y):
-  if x < 1 or x > gridSize or y < 1 or y > gridSize:
+  if x < 0 or x > gridSize or y < 0 or y > gridSize:
     raise Exception("Invalid coords: " + str(x) + ":" + str(y)); 
 
   pygame.draw.rect(screen, white, (x * (width / gridSize), y * (height / gridSize), (width / gridSize), (height / gridSize)))
 
+board = [
+  [0,0,0,0,0,0,0,0,0,0],
+  [0,1,0,0,0,0,0,0,0,0],
+  [0,0,0,0,0,0,0,0,0,0],
+  [0,0,0,0,0,0,0,0,0,0],
+  [0,0,0,0,0,1,0,0,0,0],
+  [0,0,0,0,1,1,1,0,0,0],
+  [0,0,0,0,0,1,0,0,0,0],
+  [0,0,0,0,0,0,0,0,0,0],
+  [0,0,0,0,0,0,0,0,0,1],
+  [0,0,0,0,0,0,0,0,1,1]
+]
 running = True
 while running:
   screen.fill(black)
   
-  grid()
-  fillCell(3, 3)
-  fillCell(5, 7)
+  drawGrid()
+  
+  for y in range(len(board)):
+    for x in range(len(board[y])):
+      if board[y][x] == 1:
+        fillCell(x, y)
 
   pygame.display.flip()
   for event in pygame.event.get():
